4fedfbc671a1d60a67197d445648b27c4d7bce84
Alex McMillen <amcmillen@Alexs-MacBook-Pro.local>
2016-12-28 19:29:27 -0500

    Fixing installer to pull appropriate GPG Key

--- install/install.sh
+++ install/install.sh
@@ -1,9 +1,9 @@
 yum -y install git
 echo -n "Installing the Atomic GPG keys: "
-if [ ! -f RPM-GPG-KEY.art.txt ]; then
-  wget -q https://www.atomicorp.com/RPM-GPG-KEY.art.txt 1>/dev/null 2>&1
+if [ ! -f RPM-GPG-KEY.atomicorp.txt ]; then
+  wget -q https://www.atomicorp.com/RPM-GPG-KEY.atomicorp.txt 1>/dev/null 2>&1
 fi
-rpm -import RPM-GPG-KEY.art.txt >/dev/null 2>&1
+rpm -import RPM-GPG-KEY.atomicorp.txt >/dev/null 2>&1
 cd /etc/yum.repos.d/ && wget https://raw.githubusercontent.com/sliqua-hosting/modsecurity/master/install/atomic.repo
 yum --enablerepo="atomic" install mod_security
 rm -rf /etc/httpd/conf.d/00_mod_security.conf && touch /etc/httpd/conf.d/00_mod_security.conf
